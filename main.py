db2f301f43abb3e1eeeaca513c672a3c793fa1c4
eddymck98 <eddymck98@gmail.com>
2026-08-19 20:13:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from routers import auth, participants, treasure, puzzle, admin
+from routers import auth, participants, party, treasure, puzzle, admin
 
 app = FastAPI(title="Scuffed Olympics")
 
@@ -10,6 +10,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Include all routers
 app.include_router(auth.router)
 app.include_router(participants.router)
+app.include_router(party.router)
 app.include_router(treasure.router)
 app.include_router(puzzle.router)
 app.include_router(admin.router)
